5ef864e9f85c9d40615ebec76272a80833b986dc
Любовь Беркова <grandprinceess7777@gmail.com>
2022-05-10 16:51:19 +0300

    комментарий

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 
-from django.core.exceptions import PermissionDenied
-from posts.models import Group, Post
+from posts.models import Group, Post, Comment
 from .serializers import PostSerializer, GroupSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 
@@ -14,9 +13,6 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_class = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
         serializer.save(author=self.request.user)
 
 
@@ -30,22 +26,9 @@ class CommentViewSet(viewsets.ModelViewSet):
     permission_class = [IsAuthenticated, IsAuthorOrReadOnly]
 
     def get_queryset(self):
-        post_id = self.kwargs.get('post_id')
-        post = get_object_or_404(Post, pk=post_id)
-        comments = post.comments.all()
-        return comments
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
+        return post.comments.all()
 
     def perform_create(self, serializer):
-        post_id = self.kwargs.get('post_id')
-        post = get_object_or_404(Post, pk=post_id)
+        post = get_object_or_404(Post, id=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, instance):
-        if instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(CommentViewSet, self).perform_destroy(instance)
